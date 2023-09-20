1955b355c286c8ccc28b82cbf511de3148fb44d8
DCKAP <kishorekumarcdckap@gmail.com>
2023-09-20 15:42:26 +0530

    micro task using future

---
+++ bin/microtask.dart
@@ -0,0 +1,13 @@
+void main(){
+   print("A");
+  Future((){
+ print("B");
+  Future(()=>print("C"));
+  Future.microtask(()=>print("D"));
+  Future(()=>print("E"));
+  print("F");
+  }
+ 
+  );
+  print("G");
+}
